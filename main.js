bccd93a41ffc8846fbfc262af5321d1991daf9c8
arBalasquide <adrian.balasquide@gmail.com>
2019-07-11 10:41:56 -0400

    Added main page rendering for available media

--- main.js
+++ main.js
@@ -1,43 +1,46 @@
-var express = require('express');
-var fs = require('fs');
-var stream = require('./stream');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const stream = require('./stream');
+const gathermedia = require('./gathermedia');
 
 const app = express();
 const port = 8080;
 
-var mediaList = [];
+mediapath = './media';
+extensions = ['.mp4', '.m4v'];
 
-function getMedia(){
-  fs.readdir('./media', function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        console.log(`Adding '${items[i]}' to the list of available media.`)
-        mediaList.push({
-          key:    items[i],
-          value:  fs.statSync('./media/'+items[i])
-        });
-    }
-    console.log("Gathered list of available media.");
-    mediaList.sort();
-  });
-}
+// Will find all media with supported extensions in ./media
+const filename = gathermedia.mediafilepath(mediapath, extensions);
 
-app.get('/', (req, res) => {
+app.set('view engine', 'pug');
+ 
+app.get('/', (req, res, next) => {
   console.log("request for /");
-  res.send("Main Page been Built");
+  res.render('index', {path: filename});
 })
 
+// URL use %20 as space so replace it here with " " so media can be found locally
 app.get('/*.mp4$', (req, res) => {
   console.log(`User requesting media: ${req.url}`);
-  stream.start(`.${req.url}`, "mp4", req, res);
+  stream.start(`.${req.url.replace(/%20/g, " ")}`, req, res);
+})
+app.get('/*.m4v$', (req, res) => {
+  console.log(`User requesting media: ${req.url}`);
+  stream.start(`.${req.url.replace(/%20/g, " ")}`, req, res);
+})
+
+app.get('/*.mkv$', (req, res) => {
+  console.log(`User requesting media: ${req.url}`);
+  stream.start(`.${req.url.replace(/%20/g, " ")}`, req, res);
 })
 
 // Runs only if all previously written functions fail
 app.use((req, res, next) => {
   res.send("Error 404: Page not found!");
+  console.log(`Request for unavailable page @ ${req.url}`);
 })
 
 app.listen(port, () => {
   console.log(`Server started sucesfully. Listening on port ${port}`);
 })
-
-getMedia();
