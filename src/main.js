b28f439d83de6c5e954bbcd93b0c2336d2084b51
arBalasquide <adrian.balasquide@gmail.com>
2019-07-14 12:37:38 -0400

    One app.get() route for all video extensions

--- src/main.js
+++ src/main.js
@@ -6,31 +6,29 @@ import gather from './gathermedia'
 import {PATHS, SUPPORTED_EXTENSIONS} from './constants'
 
 const app = express();
-const port = 8081;
+const port = 8080;
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res, next) => {
   console.log("request for /");
 
-  // Update available content 
+  // Update available content
   let media_titles = gather.titles(PATHS.MEDIA_PATH);
   let media_files = gather.files(media_titles);
 
   res.render('index', {titles: media_titles, files: media_files});
 })
 
-// URL use %20 as space so replace it here with " " so media can be found locally
-app.get('/*.mp4$', (req, res) => {
-  console.log(`User requesting media: ${req.url}`);
-  stream.start(`.${req.url.replace(/%20/g, " ")}`, req, res);
-})
-app.get('/*.m4v$', (req, res) => {
-  console.log(`User requesting media: ${req.url}`);
-  stream.start(`.${req.url.replace(/%20/g, " ")}`, req, res);
+// Routing for all supported video extensions
+SUPPORTED_EXTENSIONS.VIDEO.forEach(function(path){
+  app.get(`/*${path}$`, (req, res) =>{
+    console.log(`User requesting media: ${req.url}`);
+    stream.start(`.${req.url.replace(/%20/g, " ")}`, req, res);
+  })
 })
 
-// Runs only if all previously written functions fail
+// Route user to 404 page
 app.use((req, res, next) => {
   res.send("Error 404: Page not found!");
   console.log(`Request for unavailable page @ ${req.url}`);
