dddf949bda455afcf9a7b0e73a6dff1e2da24de6
arBalasquide <adrian.balasquide@gmail.com>
2020-07-19 18:37:57 -0400

    Live Transcoding temp changes

--- src/stream.js
+++ src/stream.js
@@ -1,34 +1,28 @@
-var fs = require('fs');
+import fs from 'fs'
+import ffmpeg from 'fluent-ffmpeg'
 
 exports.start = function(req, res){
 
-  const path = '.'+req.url.replace(/%20/g, ' '); // %20 = " "
-  const stats = fs.statSync(path); // stores all the stats of the file
-  const fileSize = stats.size;
-  const range = req.headers.range; // HTTP allows to send a range of data
+  const video_path = '.'+req.url.replace(/%20/g, ' ');
+  const readStream = fs.createReadStream(video_path);
 
-  if(range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-type' : `video/mp4`,
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': `video/mp4`,
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
-};
+  readStream.on('open', function() {
+    // ffmpeg stream
+    var proc = new ffmpeg(video_path)
+      .outputOptions(['-movflags isml+frag_keyframe'])
+      .toFormat('mp4')
+      .withAudioCodec('copy')
+      .on('end', function() {
+      console.log('Processing finished!');
+      })
+      .on('error', function(err,stdout,stderr) {
+        console.log('an error happened: ' + err.message);
+        console.log('ffmpeg stdout: ' + stdout);
+        console.log('ffmpeg stderr: ' + stderr);
+      })
+      .on('progress', function(progress) {
+      console.log('Processing: ' + progress.percent + '% done');
+      })
+      .pipe(res, {end: true});
+  });
+}
